3e24e4f7d0d8a3f62485fd91941c0d278f095bc3
DiptiLecturer <lecturer3.amad.nsda@dipti.com.bd>
2026-07-01 12:34:41 +0600

    "shared pref done "

--- app/src/main/java/org/freedu/myfirstprojectb8/FirstScreen.kt
+++ app/src/main/java/org/freedu/myfirstprojectb8/FirstScreen.kt
@@ -1,6 +1,7 @@
 package org.freedu.myfirstprojectb8
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -10,31 +11,42 @@ import org.freedu.myfirstprojectb8.databinding.ActivityFirstScreeenBinding
 class FirstScreen : AppCompatActivity() {
 
     private lateinit var binding: ActivityFirstScreeenBinding
+
+    private lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityFirstScreeenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE)
+
+        val savedName = sharedPreferences.getString("username", null)
+
+        if(savedName!=null){
+            Toast.makeText(this, "Data exist", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this@FirstScreen, SecondScreen::class.java))
+            finish()
+        }
+
         binding.btnSubmit.setOnClickListener {
 
             val name = binding.etName.text.toString()
 
-            if(name.isEmpty()){
+            if (name.isEmpty()) {
                 Toast.makeText(this@FirstScreen, "Name is required", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
-            }else{
-                val intent =Intent(this@FirstScreen, SecondScreen::class.java)
-                intent.putExtra("name",name)
-                startActivity(intent)
             }
-
-
-
+            val editor =sharedPreferences.edit()
+            editor.putString("username",name)
+            editor.apply()
+            val intent = Intent(this@FirstScreen, SecondScreen::class.java)
+            intent.putExtra("name", name)
+            startActivity(intent)
 
 
         }
-
 
 
     }
--- app/src/main/java/org/freedu/myfirstprojectb8/SecondScreen.kt
+++ app/src/main/java/org/freedu/myfirstprojectb8/SecondScreen.kt
@@ -1,5 +1,7 @@
 package org.freedu.myfirstprojectb8
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,16 +9,28 @@ import org.freedu.myfirstprojectb8.databinding.ActivitySecondScreenBinding
 
 class SecondScreen : AppCompatActivity() {
     private lateinit var binding: ActivitySecondScreenBinding
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivitySecondScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE)
+        val savedName = sharedPreferences.getString("username","No name")
+        val intentName = intent.getStringExtra("name")
+        binding.tvName.text = intentName
+        binding.tvName.text = "welcome $savedName"
 
-        val name =intent.getStringExtra("name")
-        binding.tvName.text = name
+        binding.btnLogout.setOnClickListener {
 
+            val editor = sharedPreferences.edit()
+            editor.clear()
+            editor.apply()
+            startActivity(Intent(this@SecondScreen, FirstScreen::class.java))
+            finish()
+
+        }
 
 
     }
